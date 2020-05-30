44398421e2b887dfe4ddc07ce51a7630e052e6ae
kianbadie <kianbadie@gmail.com>
2020-05-29 22:15:38 -0700

    Combine start scripts into ./start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Start up script for RemoteMouse
+trap 'kill %1' SIGINT
 ADDRESSES=$(hostname -I)
 ADDRESSES_ARRAY=($ADDRESSES)
 HOST=${ADDRESSES_ARRAY[0]}
 SERVER_PORT=8000
 SOCKET_PORT=8888
 python3 injectAddress.py $SOCKET_PORT $HOST
-python3 rmServer.py rmApp:app $SERVER_PORT $HOST
+python3 socketServer.py $SOCKET_PORT $HOST & python3 rmServer.py rmApp:app $SERVER_PORT $HOST
--- startSocket.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Start up script for RemoteMouse
-ADDRESSES=$(hostname -I)
-ADDRESSES_ARRAY=($ADDRESSES)
-HOST=${ADDRESSES_ARRAY[0]}
-SOCKET_PORT=8888
-python3 socketServer.py $SOCKET_PORT $HOST
